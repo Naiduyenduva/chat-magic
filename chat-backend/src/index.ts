274bc472f9b45d3e1e019a3b114aefbbabe2512b
Laxmunaidu <luckyyenduva8.7@gmail.com>
2025-03-13 17:28:10 +0530

    added room chat logic

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -1,3 +1,35 @@
-import WebSocket from "ws";
+import websocket,{WebSocket} from "ws";
 
-const wss = new WebSocket({ port:8080})
+const wss = new websocket.Server({port:8080});
+
+interface User {
+    socket: WebSocket;
+    room: string
+}
+
+let allSockets: User[] = [];
+let count = 0;
+
+wss.on("connection",(socket)=> {
+    count++
+    socket.on("message",(message:string)=> {
+        const parsedMessage = JSON.parse(message)
+        if(parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type === "chat") {
+           const currentUserRoom = allSockets.find((x)=>x.socket==socket)?.room
+           for(let i=0;i<allSockets.length;i++) {
+               if(allSockets[i].room == currentUserRoom) {
+                allSockets[i].socket.send(parsedMessage.payload.message)
+               }
+            }
+        }
+    })
+    socket.on("close",()=> {
+        console.log("chat closed/disconnected",count)
+    })
+})
